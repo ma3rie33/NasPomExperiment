9fc23610045e82abc87e7625a241dd95abd10113
Marie Lucka <ma3rie.33@gmail.com>
2020-11-12 20:25:25 +0100

    Initial commit

---
+++ src/main/java/cz/czechitas/SpousteciTrida.java
@@ -0,0 +1,9 @@
+package cz.czechitas;
+
+public class SpousteciTrida {
+
+    public static void main(String[] args) {
+        System.out.println("Zdravim, pozemstani! Zavedte me ke svemu vudci");
+    }
+
+}
